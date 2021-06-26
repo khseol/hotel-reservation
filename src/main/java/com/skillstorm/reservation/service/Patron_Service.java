4d055d05efcf605d8e443c0a643bec2489265bf8
Kathy Seol <kseol@skillstorm.com>
2021-06-26 13:08:58 -0400

    created the patron service class to call dao methods

---
+++ src/main/java/com/skillstorm/reservation/service/Patron_Service.java
@@ -0,0 +1,33 @@
+package com.skillstorm.reservation.service;
+
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.skillstorm.reservation.data.User_Information_DAO_Impl;
+import com.skillstorm.reservation.models.User_Information;
+
+/**
+ * 
+ * @author kathyhseol
+ * service class that calls the methods there implemented
+ * from the java data package.
+ * Class is specifically used for servicing the manipulation of data in the 
+ * User_Information MODEL and the USER_INFORMATION table in the DB.
+ *
+ */
+public class Patron_Service {
+	
+	User_Information_DAO_Impl patronService = new User_Information_DAO_Impl();
+	
+	public boolean saveUser (User_Information patron) throws SQLException{
+		return patronService.save(patron); //the patron object will be type casted when passing through the method execution process.
+	}
+	
+	public List<User_Information> listAllUsers()throws SQLException{
+		List<User_Information> allPatrons = patronService.findAll();
+		return allPatrons;
+		
+	}
+	
+}
